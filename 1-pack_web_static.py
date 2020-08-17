8e2e9408f1a3105d074565aa4045cd28cbeda770
mauricioolarte <reneolarte@yahoo.com>
2020-08-17 16:58:31 -0500

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" compress a file whit fabric """
+
+from fabric.api import local
+import datetime
+
+
+def do_pack():
+    """ compres a file """
+    now = datetime.datetime.now()
+    year = str(now.year)
+    month = str(now.month)
+    day = str(now.day)
+    hour = str(now.hour)
+    minute = str(now.minute)
+    second = str(now.second)
+    name_file = "web_static_" + year + month + day + hour + minute + second
+    command = "sudo tgz ./versions/" + name_file + ".tgz ./web_static"
+    print(name_file)
+    print(command)
+    local("sudo mkdir -p ./versions")
+    local(command)
