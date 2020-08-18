a1c3aa0c930af57be32b1a022ba1fbf17e756623
mauricioolarte <reneolarte@yahoo.com>
2020-08-18 00:11:15 -0500

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#this configure a dployd to server.
+# this configure a dployd to server.
 # check if nginx is already install
 if ! command -v Nginx &> /dev/null
 then
@@ -34,13 +34,13 @@ fi
 # check if a /data/web_static/shared/ exist and create it if not exist.
 if [ ! -d /data/web_static/shared/ ]
 then
-    sudo mkdir -p /data/web_static/shared/ /
+    sudo mkdir -p /data/web_static/shared/
 fi
 
 # check if a /data/ exist and create it if not exist.
-if [ ! -d /data/web_static/releases/test/  ]
+if [ ! -d /data/web_static/releases/test/ ]
 then
-    sudo mkdir -p /data/web_static/releases/test/ 
+    sudo mkdir -p /data/web_static/releases/test/
 fi
 
 # Create a fake HTML file /data/web_static/releases/test/index.html to test nginx.
@@ -64,9 +64,9 @@ sudo ln -s /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-var1="\\n\tlocation /hbnb_static {\\n\t\talias /data/web_static/current/;\n\t}"
-# sudo sed -i "/server_name _./a\ $var1" /etc/nginx/sites-enable/default
-sudo sed -i "26i $var1" /etc/nginx/sites-enable/default
+var1="\\\tlocation /hbnb_static {\\n\t\talias /data/web_static/current/;\n\t}"
+# sudo sed -i "/server_name _./a\ $var1" /etc/nginx/sites-enabled/default
+sudo sed -i "26i $var1" /etc/nginx/sites-enabled/default
 
 # updating changes innginx
 sudo service nginx restart
