cf7642142cff6a07e70a22c1d6c70385034e71b4
mauricioolarte <reneolarte@yahoo.com>
2020-08-17 22:54:04 -0500

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import *
 import os
 import datetime
 
-env.host = [35.185.14.17, 34.73.91.94]
+env.hosts = ['35.185.14.17', '34.73.91.94']
 
 
 def do_pack():
@@ -26,24 +26,27 @@ def do_pack():
         file = local(command)
         return file
     except:
+        print("error")
         return(None)
 
 
-def def do_deploy(archive_path):
+def do_deploy(archive_path):
     """ deploy files in server """
     if os.path.isfile(archive_path):
-        try:
-            put(archive_path, "/temp/")
-            destini_path = "/data/web_static/releases/" + archive_path.strip(".tgz") + "/"
-            run("mkdir -p {}".format(destini_path))
-            run("tar -xvzf /tmp/{} -C {}".format(archive_path.split("/")[-1], destini_path))
-            run("rm /tmp/{}".format(archive_path.split("/")[-1]))
-            run("mv {}/web_static/* {}".format(destini_path, destini_path))
-            run("rm {}/web_static".format(destini_path))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current")
-            return(True)
-        except:
-            return(False)
+                  
+        put(archive_path, '/tmp/')
+        
+        destini_path = "/data/web_static/releases/" + archive_path.strip(".tgz") + "/"
+        run("mkdir -p {}".format(destini_path))
+        run("tar -xvzf /tmp/{} -C {}".format(archive_path.split("/")[-1], destini_path))
+        run("rm /tmp/{}".format(archive_path.split("/")[-1]))
+        run("mv -f {}web_static/* {}".format(destini_path, destini_path))
+
+        run("rm -rf {}web_static".format(destini_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current")
+        return(True)
+        
     else:
+        print("error")
         return(False)
