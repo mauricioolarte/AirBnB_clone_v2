7c1a3cc37a2b5b227a968bd3b02c86bdf7a9e01f
mauricioolarte <reneolarte@yahoo.com>
2020-09-02 20:01:27 +0000

    web_flask/7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" this module starts a flask aplication """
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+# @app.teardown_appcontext
+# def close_db_sesion():
+#     storage.close(app)
+
+@app.route('/states_list')
+def list_states():
+    query = storage.all(State)
+    for row in query:
+        print(row.__dict__['name'])
+    return (render_template('7-states_list.html', query=query))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
